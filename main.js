ca3382e1ed3ac0b86f3d6d30a738ea4000a706e3
zgulll <caeshmeire@gmail.com>
2026-01-19 07:49:21 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("root").innerHTML = `
+  <div style="font-family: Arial; text-align: center; margin-top: 50px;">
+    <h1>Know Your Politician – Kashmir</h1>
+    <p>Welcome! This website will help you learn about your representatives.</p>
+    <p>🚧 Work in progress 🚧</p>
+  </div>
+`;
